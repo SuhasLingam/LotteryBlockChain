5dc0532ed5eede1c53cfce90c787497142b041b6
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-12-03 00:43:44 +0530

    :heavy_check_mark: first Contract

--- Blockchain/contracts/Lottery.sol
+++ Blockchain/contracts/Lottery.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract lottery {}
+contract lottery {
+    address public manager;
+    address payable[] public participants;
+
+    constructor() {
+        manager = payable(msg.sender);
+    }
+
+    function showBal() public view returns (uint) {
+        return address(this).balance;
+    }
+}
