6cc44a08586af4548ad6f04df4c570c9df83372b
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-12-04 01:41:51 +0530

    Succecfully deployed Contract to Sepolia testnet :white:check:mark:

--- Blockchain/hardhat.config.js
+++ Blockchain/hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Go to https://alchemy.com, sign up, create a new App in
+// its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "xBo7c9nK2wg5LexoeEalUs9v7EjT4KT4";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY =
+  "e975d8facced011295a5758b10e5bfd261ec767e1c8f5f2ebe80df94716b5e3d";
+
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
--- Blockchain/scripts/deploy.js
+++ Blockchain/scripts/deploy.js
@@ -1,28 +1,12 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Lottery = await hre.ethers.getContractFactory("lottery");
+  const lottery = await Lottery.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await lottery.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Lottery Deployed to :", await lottery.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -31,3 +15,5 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+//0x0eb56B3cbc98BafcD5B00957979479ccc5dcE6B5
